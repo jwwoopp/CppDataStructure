bd4d5f8a3c31381fcc55feea17beaf1effac01d9
jwwoopp <jwwoopp@gmail.com>
2026-08-21 10:53:17 +0900

    내 코드 백업

---
+++ List/Main.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include "List.h"
+#include <string>
+
+int main()
+{
+	// 캐릭터 리스트.
+	List<std::string> characterList;
+	characterList.PushBack("Warrior");
+	characterList.PushBack("Knight");
+	characterList.PushBack("Archer");
+
+	// 복사 생성.
+	List<std::string> copiedList(characterList);
+	copiedList[0] = "BlackKnight";
+
+	// 이동 생성.
+	List<std::string> copiedList2(std::move(copiedList));
+
+	// 범위-기반 루프로 출력.
+	for (const auto& value : characterList)
+	{
+		std::cout << value << "\n";
+	}
+
+	// 이터레이터 기반 루프 순회.
+	for (auto iterator = characterList.begin();
+		iterator != characterList.end();
+		++iterator)
+	{
+		std::cout << (*iterator) << "\n";
+	}
+}
