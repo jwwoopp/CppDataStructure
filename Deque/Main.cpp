cda9fedc6f0c929eb997860fb80cf02f11de78d6
jwwoopp <jwwoopp@gmail.com>
2026-08-21 12:01:29 +0900

    Dequeue 실습

---
+++ Deque/Main.cpp
@@ -0,0 +1,25 @@
+#include "Deque.h"
+
+#include <iostream>
+#include <queue>
+#include <stack>
+
+int main()
+{
+	Deque<int> deque;
+
+		for (int ix = 0; ix <= 10; ++ix)
+		{
+			if (ix % 2 == 0)
+			{
+				deque.AddFront(ix);
+			}
+			else
+			{
+				deque.AddRear(ix);
+			}
+		}
+
+	deque.Print();
+
+}
